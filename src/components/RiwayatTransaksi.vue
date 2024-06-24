ae82fbbd58adaf95297108c58334a6a67c7da2e6
AlvynoPB <alvyno.putra.b@gmail.com>
2024-06-25 00:04:14 +0700

    25 Juni 1

---
+++ src/components/RiwayatTransaksi.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <UserHeader />
+    <h1>Riwayat Transaksi</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Pemesan</th>
+          <th>Alamat</th>
+          <th>Produk</th>
+          <th>Waktu Pesan</th>
+          <th>Catatan</th>
+          <th>Keterangan</th>
+          <th>Aksi</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="transaction in filteredRiwayatTransaksi"
+          :key="transaction.id"
+        >
+          <td>{{ transaction.id }}</td>
+          <td>{{ transaction.user }}</td>
+          <td>{{ transaction.address || getUserAddress(transaction.user) }}</td>
+          <td>
+            <ul>
+              <li v-for="item in transaction.items" :key="item.id">
+                {{ item.name }} - ({{ item.quantity }})
+              </li>
+            </ul>
+          </td>
+          <td>{{ new Date(transaction.timestamp).toLocaleString() }}</td>
+          <td>{{ transaction.catatan }}</td>
+          <td>{{ transaction.description || transaction.descriptions }}</td>
+          <td>
+            <button @click="deleteTransactionHistory(transaction.id)">
+              Hapus
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { mapState } from "vuex";
+import UserHeader from "@/components/UserHeader.vue";
+
+export default {
+  name: "RiwayatTransaksi",
+  components: {
+    UserHeader,
+  },
+  data() {
+    return {
+      userData: {},
+    };
+  },
+  computed: {
+    ...mapState(["riwayatTransaksi"]),
+    filteredRiwayatTransaksi() {
+      const user = JSON.parse(localStorage.getItem("user-info"));
+      return this.riwayatTransaksi.filter((transaction) =>
+        transaction.items.some((item) => item.pedagang === user.NamaWarung)
+      );
+    },
+  },
+  created() {
+    this.fetchUserData();
+    this.fetchRiwayatTransaksi();
+  },
+  methods: {
+    fetchUserData() {
+      axios
+        .get("http://localhost:3000/User")
+        .then((response) => {
+          this.userData = response.data.reduce((acc, user) => {
+            acc[user.Nama] = user.Alamat;
+            return acc;
+          }, {});
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+    },
+    fetchRiwayatTransaksi() {
+      axios
+        .get("http://localhost:3000/RiwayatTransaksi")
+        .then((response) => {
+          this.$store.commit("SET_RIWAYAT_TRANSAKSI", response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching transaction history:", error);
+        });
+    },
+    getUserAddress(userName) {
+      return this.userData[userName] || "Unknown Address";
+    },
+    deleteTransactionHistory(transactionId) {
+      axios
+        .delete(`http://localhost:3000/RiwayatTransaksi/${transactionId}`)
+        .then(() => {
+          this.$store.commit("REMOVE_TRANSACTION_HISTORY", transactionId);
+          console.log(
+            `Transaction ${transactionId} deleted successfully from history`
+          );
+        })
+        .catch((error) => {
+          console.error("Error deleting transaction history:", error);
+        });
+    },
+  },
+};
+</script>
---
+++ src/components/UserUpdateProduk.vue
@@ -0,0 +1,232 @@
+<template>
+    <div>
+      <Header />
+      <h1>Update Produk</h1>
+      <div class="update-container">
+        <img
+          v-if="DataProduk.imageUrl"
+          :src="DataProduk.imageUrl"
+          alt="Product Image"
+          class="product-image"
+        />
+        <form class="update" @submit.prevent="updateProduk">
+          <input
+            type="text"
+            name="Nama"
+            placeholder="Ubah Nama Produk"
+            v-model="DataProduk.Nama"
+          />
+          <input
+            type="text"
+            name="Harga"
+            placeholder="Ubah Harga Produk"
+            v-model="DataProduk.Harga"
+          />
+          <select v-model="DataProduk.Kategori">
+            <option disabled value="">Pilih Kategori</option>
+            <option
+              v-for="kategori in kategoriList"
+              :key="kategori.id"
+              :value="kategori.Kategori"
+            >
+              {{ kategori.Kategori }}
+            </option>
+          </select>
+          <input
+            type="text"
+            name="Keterangan"
+            placeholder="Ubah Keterangan Produk"
+            v-model="DataProduk.Keterangan"
+          />
+          <input
+            type="text"
+            name="Stok"
+            placeholder="Ubah Stok Produk"
+            v-model="DataProduk.Stok"
+          />
+          <input type="file" @change="onImageChange" />
+          <button type="submit">Update Data Produk</button>
+        </form>
+        <form class="delete">
+          <button class="delete-btn" @click="deleteProduk">Hapus Produk</button>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Header from "./Header.vue";
+  import axios from "axios";
+  
+  export default {
+    name: "UserUpdateProduk",
+    components: {
+      Header,
+    },
+    data() {
+      return {
+        DataProduk: {
+          id: "",
+          Nama: "",
+          Harga: "",
+          Kategori: "",
+          Keterangan: "",
+          Pedagang: "",
+          Stok: "",
+          imageUrl: "",
+        },
+        kategoriList: [],
+        warungList: [],
+        imageFile: null,
+      };
+    },
+    methods: {
+      async updateProduk() {
+        try {
+          if (this.imageFile) {
+            const formData = new FormData();
+            formData.append("image", this.imageFile);
+            const response = await axios.post(
+              "http://localhost:3001/uploads",
+              formData
+            );
+            this.DataProduk.imageUrl = `http://localhost:3001${response.data.imageUrl}`;
+          }
+          const result = await axios.put(
+            `http://localhost:3000/DataProduk/${this.DataProduk.id}`,
+            this.DataProduk
+          );
+          if (result.status === 200) {
+            this.$router.push({ name: "Dagangan" });
+          }
+        } catch (error) {
+          console.error("Error updating product:", error);
+          alert(
+            "An error occurred while updating the product. Please try again later."
+          );
+        }
+      },
+      async fetchKategori() {
+        try {
+          const response = await axios.get("http://localhost:3000/DataKategori");
+          this.kategoriList = response.data;
+        } catch (error) {
+          console.error("Error fetching categories:", error);
+        }
+      },
+     
+      async fetchProduct() {
+        try {
+          const productId = this.$route.params.id;
+          const response = await axios.get(
+            `http://localhost:3000/DataProduk/${productId}`
+          );
+          this.DataProduk = response.data;
+        } catch (error) {
+          console.error("Error fetching product:", error);
+        }
+      },
+      onImageChange(event) {
+        this.imageFile = event.target.files[0];
+      },
+      async deleteProduk() {
+        try {
+          const result = await axios.delete(
+            `http://localhost:3000/DataProduk/${this.DataProduk.id}`
+          );
+          if (result.status === 204) {
+            this.$router.push({ name: "Dagangan" });
+          }
+        } catch (error) {
+          console.error("Error deleting product:", error);
+          alert(
+            "An error occurred while deleting the product. Please try again later."
+          );
+        }
+      },
+    },
+    mounted() {
+      this.fetchKategori();
+ 
+      this.fetchProduct();
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .update-container {
+    max-width: 800px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .product-image {
+    width: 50%;
+    height: auto;
+    margin-bottom: 20px;
+  }
+  
+  .update {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+  }
+  
+  .update input,
+  .update select,
+  .update button {
+    display: block;
+    margin-bottom: 10px;
+    padding: 10px;
+    width: 400px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+  
+  .update input::placeholder,
+  .update select::placeholder {
+    color: #aaa;
+  }
+  
+  .update button {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+  
+  .update button:hover {
+    background-color: #0056b3;
+  }
+  
+  .delete {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+  }
+  
+  .delete-btn {
+    background-color: red;
+    display: block;
+    padding: 10px;
+    width: 400px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+  
+  .delete-btn:hover {
+    background-color: #c82333;
+  }
+  </style>
+  
